7ab550c1d0f33710802e9baa5078b5ec974856b8
Andrei Aksenov <aksenovaa@hotmail.com>
2022-04-10 18:04:44 +0200

    Correct devide

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -8,15 +8,15 @@ public class Calculator {
     public BinaryOperator<Integer> minus = (x, y) -> x - y;
     public BinaryOperator<Integer> multiply = (x, y) -> x * y;
     //public BinaryOperator<Integer> devide = (x, y) -> x / y; //If y ==0 trows exception divide by zero
-//    public BinaryOperator<Integer> devide = (x, y) -> y==0 ? 0 : x / y; //Вариант простой
-    public BinaryOperator<Integer> devide = (x, y) -> { //Немного извращенный вариант
+    public BinaryOperator<Integer> devide = (x, y) -> y==0 ? 0 : x / y; //Вариант простой
+    /*public BinaryOperator<Integer> devide = (x, y) -> { //Немного извращенный вариант
         try {
             return x / y;
         } catch (ArithmeticException e){
             System.out.println("Деление на 0");
         }
         return 0;
-    };
+    };*/
     public UnaryOperator<Integer> pow = x -> x * x;
     public UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
     public Predicate<Integer> isPositive = x -> x > 0;
